0306fcd75d78462af72a59c7565ae7e3484df113
ViniciusPii <vinysales@hotmail.com>
2020-08-16 15:59:07 -0300

    [Navegação] Inicio da navegação

--- app/src/main/java/com/viniciuspiih/motivation/SplashActivity.kt
+++ app/src/main/java/com/viniciuspiih/motivation/SplashActivity.kt
@@ -1,15 +1,40 @@
 package com.viniciuspiih.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
+        }
+
+        buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val id = v?.id
+
+        if (id == R.id.buttonSave) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = editName.text.toString()
+
+        if (name != "") {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Preencha seu Nome!", Toast.LENGTH_SHORT).show()
         }
     }
 }
