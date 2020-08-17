8b430ea62bd88505f9f37f866d949e67e65a9430
ViniciusPii <vinysales@hotmail.com>
2020-08-17 18:12:28 -0300

    [UI] Implementação dos Filtros personalizados

--- app/src/main/java/com/viniciuspiih/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/viniciuspiih/motivation/infra/MotivationConstants.kt
@@ -4,4 +4,10 @@ class MotivationConstants private constructor() {
     object KEY {
         const val PERSON_NAME = "name"
     }
+
+    object PHRASEFILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val MORNING = 3
+    }
 }
